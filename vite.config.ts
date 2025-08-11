4de47d0b734f98428fe16d78e47ec7774862d86a
Jos Warmer <jos.warmer@openmodeling.nl>
2025-08-11 16:08:48 +0200

    Update to flowbite webapp

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
